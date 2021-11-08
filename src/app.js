7a823bf3c9d2387a46e659edba711f70c7c206f0
Alisha Tiwari <alishatiwari24@gmail.com>
2021-11-08 14:57:58 +0530

    fixed: health API

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import { connectToDatabase } from './helpers/connectToDatabase.helper.js';
+import express from "express";
+import { connectToDatabase } from "./helpers/connectToDatabase.helper.js";
 
 // TASK-003 Move PORT to environment file
 const PORT = 54321;
@@ -9,11 +9,10 @@ export const app = express();
 export async function startServer() {
   await connectToDatabase();
 
-  app.get('/health', (req, res) => {
-    console.log('GET /health');
+  app.get("/health", (req, res) => {
+    console.log("GET /health");
     // TASK-001 Fix health route. The above console is printing but the response is not sent to client.
-
-    return res.status(200);
+    return res.status(200).send({ message: "Server is in good health!!!" });
   });
 
   app.listen(PORT, async () => {
