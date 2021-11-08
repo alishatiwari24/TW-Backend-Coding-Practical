42bb85a2ae2fde9d6807cd13fccbae3e68e50917
Alisha Tiwari <alishatiwari24@gmail.com>
2021-11-08 15:37:10 +0530

    added: Mongodb connection

--- src/helpers/connectToDatabase.helper.js
+++ src/helpers/connectToDatabase.helper.js
@@ -1,3 +1,7 @@
+import mongoose from "mongoose";
+
 export async function connectToDatabase() {
   // TASK-002 Connect to Database.
+  await mongoose.connect(process.env.MONGO_URL);
+  console.log("MongoDB connected!!!");
 }
