bb7eddc2b0012f0937cd69be6739b5e4178e702f
Alisha Tiwari <alishatiwari24@gmail.com>
2021-11-15 15:55:01 +0530

    define: rides model schema

--- src/models/rides.model.js
+++ src/models/rides.model.js
@@ -1,8 +1,28 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
 // TASK-004 - Define rides model
-const schema = new Schema({});
+const schema = new Schema({
+  start: {
+    lat: { type: Number, required: true },
+    long: { type: Number, required: true }
+  },
+  end: {
+    lat: { type: Number, required: true },
+    long: { type: Number, required: true }
+  },
+  driver: {
+    name: { type: String, required: true },
+    vehicleNumber: { type: String, required: true },
+    vehicleType: { type: String, enum: ["CAR", "BIKE"] }
+  },
+  customers: {
+    type: Array,
+    schema: [{ name: { type: String, required: true } }]
+  },
+  createdAt: { type: String, default: new Date().toISOString() },
+  updatedAt: { type: String, default: new Date().toISOString() }
+});
 
-export const Ride = mongoose.model('rides', schema, 'rides');
+export const Ride = mongoose.model("rides", schema, "rides");
