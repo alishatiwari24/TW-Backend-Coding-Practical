a9c35feb7872a5693bb53e5872f1fbd31aa3d071
Alisha Tiwari <alishatiwari24@gmail.com>
2021-11-15 19:21:20 +0530

    add: API to update an existing ride

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { connectToDatabase } from "./helpers/connectToDatabase.helper.js";
 import { ridesController } from "./controller/rides.js";
+import bodyParser from "body-parser";
 
 export const app = express();
 
@@ -9,6 +10,9 @@ export async function startServer() {
   // TASK-003 Move PORT to environment file
   const PORT = process.env.SERVER_PORT;
 
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+
   app.get("/health", (req, res) => {
     console.log("GET /health");
     // TASK-001 Fix health route. The above console is printing but the response is not sent to client.
@@ -16,6 +20,7 @@ export async function startServer() {
   });
 
   app.get("/rides", (req, res) => ridesController.getRides(req, res));
+  app.put("/rides/:id", (req, res) => ridesController.updateRide(req, res));
 
   app.listen(PORT, async () => {
     console.log(`Listening on port ${PORT}`);
